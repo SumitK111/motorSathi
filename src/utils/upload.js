01ef59d883d15fb13c2415d5a7ab14e0c3c73136
Sumit Kumar <sumit.kumar@yolobus.in>
2025-02-02 18:21:49 +0530

    upload pdf code added

---
+++ src/utils/upload.js
@@ -0,0 +1,31 @@
+const multer = require("multer");
+const { v2: cloudinary } = require("cloudinary");
+const fs = require("fs");
+const path = require("path");
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+  })
+
+  const storage = multer.diskStorage({
+    destination: "uploads/", // Save temporarily before Cloudinary upload
+    filename: (req, file, cb) => {
+      const ext = path.extname(file.originalname); // Get original file extension
+      const uniqueName = Date.now() + "-" + Math.round(Math.random() * 1e9) + ext;
+      cb(null, uniqueName);
+    },
+  });
+  const upload = multer({
+    storage: storage,
+    limits: { fileSize: 500 * 1024 }, // 5MB size limit
+    fileFilter: (req, file, cb) => {
+      if (file.mimetype !== "application/pdf") {
+        return cb(new Error("Only PDF files are allowed!"), false);
+      }
+      cb(null, true);
+    },
+  });
+
+module.exports = {upload,cloudinary}
